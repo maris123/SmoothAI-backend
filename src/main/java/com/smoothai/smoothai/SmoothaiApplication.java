1a07e7482169032d8a7fb67ece1d743de839bfec
Anonymous-Coward <florin.jurcovici@gmail.com>
2018-05-11 19:11:42 +0300

    Created spring boot with only hateoas as dependency

---
+++ src/main/java/com/smoothai/smoothai/SmoothaiApplication.java
@@ -0,0 +1,12 @@
+package com.smoothai.smoothai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmoothaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmoothaiApplication.class, args);
+	}
+}
