1ff227e904eb03c99ac73735667c86c782483027
Maris <maris.alex93@gmail.com>
2018-05-12 18:03:12 +0300

    Changed darknet path in conf

--- src/main/java/com/smoothai/smoothai/recognizer/PythonScriptRecognizerService.java
+++ src/main/java/com/smoothai/smoothai/recognizer/PythonScriptRecognizerService.java
@@ -1,6 +1,7 @@
 package com.smoothai.smoothai.recognizer;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Path;
@@ -12,7 +13,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class PythonScriptRecognizerService implements ImageRecognizerService {
 
-	@Value("${smoothai.darknet.app}")
+	@Value("${smoothai.darknet.path}")
 	private Path darknetApp;
 
 	@Override
@@ -25,8 +26,9 @@ public class PythonScriptRecognizerService implements ImageRecognizerService {
 		StringBuilder output = new StringBuilder();
 
 		try {
-			ProcessBuilder processBuilder = new ProcessBuilder(darknetApp.toString(), "detect", "cfg/yolov3.cfg",
+			ProcessBuilder processBuilder = new ProcessBuilder("./darknet", "detect", "cfg/yolov3.cfg",
 					"yolov3.weights", filePath.toString());
+			processBuilder.directory(new File(darknetApp.toString()));
 			Process process = processBuilder.start();
 
 			BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
