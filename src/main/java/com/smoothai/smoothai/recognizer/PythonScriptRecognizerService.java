209038688693c07b236b8e1d7ed0506598fb1a58
Maris <maris.alex93@gmail.com>
2018-05-12 17:37:28 +0300

    Added service annotation

--- src/main/java/com/smoothai/smoothai/recognizer/PythonScriptRecognizerService.java
+++ src/main/java/com/smoothai/smoothai/recognizer/PythonScriptRecognizerService.java
@@ -7,7 +7,9 @@ import java.nio.file.Path;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PythonScriptRecognizerService implements ImageRecognizerService {
 
 	@Value("smoothai.recognition.script.path")
